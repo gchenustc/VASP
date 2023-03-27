b33746dfbb1568707ff7564947d388fa0ab526cf
‘gchen’ <gchenn@mail.ustc.edu.cn>
2023-03-27 23:40:51 +0800

    updata

--- 00_脚本/06_晶体平均键长/AveBondLen.py
+++ 00_脚本/06_晶体平均键长/AveBondLen.py
@@ -5,6 +5,8 @@ from ase.calculators.vasp import Vasp
 from ase.build.supercells import make_supercell
 import numpy as np
 import time
+from pymatgen.core.periodic_table import Element
+# print(Element("H").atomic_radius_calculated)  # 计算原子半径
 """
 描述: vasp提供CONTCAR即可
 
@@ -22,12 +24,13 @@ start_time = time.perf_counter()
 #atoms = calc.get_atoms()
 
 atoms = read("CONTCAR", format="vasp")
-n_atoms = len(atoms.get_atomic_numbers()) # 原子数
-#>>> 扩两倍胞，因为只计算在当前周期内的键长，边界上两原子成键距离会被算长
-P = np.zeros((3,3))
-np.fill_diagonal(P,[2,2,2])
-atoms = make_supercell(atoms, P)
-#<<<
+n_atoms = len(atoms.get_atomic_numbers())  # 原子数
+# >>> 扩两倍胞，因为只计算在当前周期内的键长，边界上两原子成键距离会被算长
+#P = np.zeros((3, 3))
+#np.fill_diagonal(P, [1, 1, 1])
+#atoms = make_supercell(atoms, P)
+# <<<
+#print(dir(atoms))
 
 # 获得成键电子
 bondpairs = get_bondpairs(atoms)
@@ -38,19 +41,29 @@ bondpairs:
 """
 
 dis_list = {}
-for a0,a1,index in bondpairs:
-    # 过滤超过一个周期的原子(为什么减1？因为Python索引从0开始)
-    if a0>=n_atoms-1 or a1>=n_atoms-1:
+for a0, a1, index in bondpairs:
+    # 下面的判断是过滤超过一个周期的原子(为什么减1？因为Python索引从0开始)
+    # if a0>n_atoms-1 or a1>n_atoms-1:
+    #    continue
+
+    dis = atoms.get_distance(a0, a1)
+    
+    symbol1=atoms.get_chemical_symbols()[a0]
+    symbol2=atoms.get_chemical_symbols()[a1]
+    _bias=0.4
+    bond_len_limit = Element(symbol1).atomic_radius_calculated + Element(symbol1).atomic_radius_calculated + _bias
+    #print(bond_len_limit)
+    if dis > bond_len_limit:
         continue
-    dis = atoms.get_distance(a0,a1)
-    key = '-'.join(sorted([atoms[a0].symbol,atoms[a1].symbol]))
+    # print(a0,a1,dis)
+    key = '-'.join(sorted([atoms[a0].symbol, atoms[a1].symbol]))
     dis_list[key] = dis_list.setdefault(key, []) + [dis]
 
 ave = 0
-n_total = sum(map(lambda x:len(x),dis_list.values()))
-print(n_total)
+n_total = sum(map(lambda x: len(x), dis_list.values()))
+# print(n_total)
 print("average per kind:\n")
-for key,value in dis_list.items():
+for key, value in dis_list.items():
     mean_ = np.array(value).mean()
     #print(n_total, value)
     ave += mean_ * (len(value)/n_total)
